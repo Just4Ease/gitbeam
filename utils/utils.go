0abf8ba338c6eea5d91e9deee0958b18a1328efe
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2024-07-20 09:50:05 +0100

    Added unpack to codebase

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"gitbeam/models"
 	"net/http"
@@ -24,4 +25,28 @@ func WriteHTTPSuccess(w http.ResponseWriter, message string, data any) {
 		Message: message,
 		Data:    data,
 	})
+}
+
+func UnPack(in interface{}, target interface{}) error {
+	var e1 error
+	var b []byte
+	switch in := in.(type) {
+	case []byte:
+		b = in
+	// Do something.
+	default:
+		// Do the rest.
+		b, e1 = json.Marshal(in)
+		if e1 != nil {
+			return e1
+		}
+	}
+
+	buf := bytes.NewBuffer(b)
+	enc := json.NewDecoder(buf)
+	enc.UseNumber()
+	if err := enc.Decode(&target); err != nil {
+		return err
+	}
+	return nil
 }
