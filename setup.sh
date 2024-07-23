d379aced7408bef1b500835776af661a2c4992ec
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2024-07-23 13:53:37 +0100

    Added docker compose

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Just4Ease/gitbeam.repo.manager ../gitbeam.repo.manager
+cd ../gitbeam.repo.manager && git submodule update --init --recursive --remote --checkout --force --rebase --recursive || exit;
+cd - || exit;
+
+
 
 git clone https://github.com/Just4Ease/gitbeam.commit.monitor ../gitbeam.commit.monitor
+cd ../gitbeam.repo.manager && git submodule update --init --recursive --remote --checkout --force --rebase --recursive || exit;
+cd - || exit;
 
 docker-compose up
