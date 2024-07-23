93fedbd032b87c8894a4153c40e71230ba01a852
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2024-07-23 14:02:52 +0100

    feat: updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+rm -rf ../gitbeam.repo.manager || exit;
 git clone https://github.com/Just4Ease/gitbeam.repo.manager ../gitbeam.repo.manager
 cd ../gitbeam.repo.manager && git submodule update --init --recursive --remote --checkout --force --rebase --recursive || exit;
 cd - || exit;
 
 
-
+rm -rf ../gitbeam.commit.monitor || exit;
 git clone https://github.com/Just4Ease/gitbeam.commit.monitor ../gitbeam.commit.monitor
 cd ../gitbeam.repo.manager && git submodule update --init --recursive --remote --checkout --force --rebase --recursive || exit;
 cd - || exit;
