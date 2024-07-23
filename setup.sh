a99d7d66371998cfcf99993ed50dcc8cf8c64be1
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2024-07-23 13:41:43 +0100

    feat: added docker compose

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Just4Ease/gitbeam.repo.manager ../gitbeam.repo.manager
+
+git clone https://github.com/Just4Ease/gitbeam.commit.monitor ../gitbeam.commit.monitor
+
+docker-compose up
